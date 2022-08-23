378d06f256cb2c0f153f60541741d9be1c7f8b50
AshikurEfty <ashikurefty713@gmai.com>
2022-08-23 23:14:14 +0600

    added main style

--- js/best.js
+++ js/best.js
@@ -1,1 +1,158 @@
-console.log('added best.js');
+// ======== select player js part ========
+const playerList = document.getElementById("added-main");
+document.getElementById('select-button-one').addEventListener('click', function(){
+  document.getElementById('select-button-one').disabled = 'true';
+  if(playerList.childElementCount < 5){
+    const node = document.createElement("li");
+    const textnode = document.getElementById("messi-name");
+    node.textContent= textnode.textContent;
+    playerList.appendChild(node);
+  }
+  else{
+    alert('You can select only 5 Players')
+  }
+})
+document.getElementById('select-button-two').addEventListener('click', function(){
+  document.getElementById('select-button-two').disabled = 'true';
+  if(playerList.childElementCount < 5){
+    const node = document.createElement("li");
+    const textnode = document.getElementById("neymar-name");
+    node.textContent= textnode.textContent;
+    playerList.appendChild(node);
+  }
+  else{
+    alert('You can select only 5 Players')
+  }
+    
+})
+document.getElementById('select-button-three').addEventListener('click', function(){
+  document.getElementById('select-button-three').disabled = 'true';
+  if(playerList.childElementCount < 5){
+    const node = document.createElement("li");
+    const textnode = document.getElementById("kylian-name");
+    node.textContent= textnode.textContent;
+    playerList.appendChild(node);
+  }
+  else{
+    alert('You can select only 5 Players')
+  }
+});   
+document.getElementById('select-button-four').addEventListener('click', function(){
+  document.getElementById('select-button-four').disabled = 'true';
+  if(playerList.childElementCount < 5){
+    const node = document.createElement("li");
+    const textnode = document.getElementById("victor-name");
+    node.textContent= textnode.textContent;
+    playerList.appendChild(node);
+  }
+  else{
+    alert('You can select only 5 Players')
+  }
+});
+document.getElementById('select-button-five').addEventListener('click', function(){
+  document.getElementById('select-button-five').disabled = 'true';
+  if(playerList.childElementCount < 5){
+    const node = document.createElement("li");
+    const textnode = document.getElementById("sergio-name");
+    node.textContent= textnode.textContent;
+    playerList.appendChild(node);
+  }
+  else{
+    alert('You can select only 5 Players')
+  }
+});
+document.getElementById('select-button-six').addEventListener('click', function(){
+  if(playerList.childElementCount < 5){
+    const node = document.createElement("li");
+    const textnode = document.getElementById("efty-name");
+    node.textContent= textnode.textContent;
+    playerList.appendChild(node);
+  }
+  else{
+    alert('You can select only 5 Players')
+  }
+});
+
+
+
+// function addedName(collectName){
+//   const createElement = document.createElement('ol');
+//   createElement.appendChild('li');
+//   const addedName = document.getElementById('added-name');
+//   const collectNameString = collectName.textContent;
+//   const previousCollectName = collectNameString;
+//   addedName.innerText= previousCollectName;
+// }
+
+//   document.getElementById('select-button-one').addEventListener('click', function(){
+//     document.getElementById('select-button-one').disabled = 'true';
+//     const collectName = document.getElementById('messi-name');
+//     addedName(collectName);
+//     DisableNextButton();
+//   })
+
+//   document.getElementById('select-button-two').addEventListener('click', function(){
+//     document.getElementById('select-button-two').disabled = 'true';
+//     const collectName = document.getElementById('neymar-name');
+//     addedName(collectName);
+//     DisableNextButton();
+//   })
+
+//   document.getElementById('select-button-three').addEventListener('click', function(){
+//     document.getElementById('select-button-three').disabled = 'true';
+//     const collectName = document.getElementById('kylian-name');
+//     addedName(collectName);
+//   })
+//   document.getElementById('select-button-four').addEventListener('click', function(){
+//     document.getElementById('select-button-four').disabled = 'true';
+//     const collectName = document.getElementById('victor-name');
+//     addedName(collectName);
+//   })
+//   document.getElementById('select-button-five').addEventListener('click', function(){
+//     document.getElementById('select-button-five').disabled = 'true';
+//     const collectName = document.getElementById('sergio-name');
+//     addedName(collectName);
+//   })
+//   document.getElementById('select-button-six').addEventListener('click', function(){
+//     document.getElementById('select-button-six').disabled = 'true';
+//     const collectName = document.getElementById('efty-name');
+//     addedName(collectName);
+//     })
+
+  // =========== calculate js part =============
+
+function getCalculateTotalPrice(){
+  const inputField = document.getElementById('price-input-field');
+  const calculatePrice = inputField.value;
+    const newCalculatePrice = parseFloat(calculatePrice);
+    const totalNewCalculatePrice = newCalculatePrice * playerList.childElementCount;
+    inputField.value = '';
+    
+    const expensesPrice = document.getElementById('expenses-price');
+    expensesPrice.innerText = totalNewCalculatePrice ;
+}
+
+  document.getElementById('calculate').addEventListener('click', function(){
+    getCalculateTotalPrice();
+  })
+
+  document.getElementById('calculate-total').addEventListener('click', function(){
+    const managerInputField = document.getElementById('manager-input-field');
+    const managerPriceString = managerInputField.value;
+    const newManagerPrice = parseFloat(managerPriceString);
+    managerInputField.value = '';
+    
+    const coachInputField = document.getElementById('coach-input-field');
+    const coachPriceString = coachInputField.value;
+    const newcoachPrice = parseFloat(coachPriceString);
+    coachInputField.value = '';
+  
+    const mainPrice = document.getElementById('expenses-price');
+    const mainpriceString = mainPrice.innerText;
+    const mainPriceNew = parseFloat(mainpriceString);
+
+    const totalPrice = newManagerPrice + newcoachPrice + mainPriceNew;
+    const mainTotalPrice = document.getElementById('total-price');
+    mainTotalPrice.innerText = totalPrice;
+  })
+
